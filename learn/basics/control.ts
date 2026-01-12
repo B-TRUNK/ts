f3980b593d1a001fe98349afdbfb2f2b3f1a6210
B-TRUNK <abanoub.boshra.anis@gmail.com>
2026-01-12 21:24:26 +0200

    basics

---
+++ learn/basics/control.ts
@@ -0,0 +1,6 @@
+let a: number = 11;
+
+if(a % 2 == 0)
+    console.log("Number is Even!");
+else
+    console.log("OOPS, Number is Odd!");
---
+++ learn/basics/loops.ts
@@ -0,0 +1,6 @@
+let a: number = 10
+
+// basic for loop
+for(a; a != 0; a--){
+    console.log(a);
+}
